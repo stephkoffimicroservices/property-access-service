a91e2a6404f3202fc27b4ed3772bc7d93a65a012
stephkoffimicroservices <stephkoffimicroservices@gmail.com>
2021-04-13 00:48:25 +0100

    Project just after creation

---
+++ src/test/java/com/stephane/propertyaccessservice/PropertyAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stephane.propertyaccessservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyAccessServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
